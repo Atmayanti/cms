074da4d7df4abd7f09de886dd43a5c416fe8aa58
Atmayanti <mayablue74@gmail.com>
2022-03-09 21:19:49 +0700

    fixing controller - js 4

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,10 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = DB::table('posts')->get(); 
-        $post_pages = DB::table('posts')->paginate(3); 
-        // return view('shop', ['posts' => Post::index(), 'post_pages' => Post::pagin()]); 
-        return view('shop', ['posts' => $posts, 'post_pages' => $post_pages]); 
+        return view('shop', ['posts' => Post::index(), 'post_pages' => Post::pagin()]); 
     }
 }
 
